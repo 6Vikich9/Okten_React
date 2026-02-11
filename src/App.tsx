3fcfa6c21c1d8a80672a51bea097b154a74f5461
6Vikich9 <v.hnetsko@gmail.com>
2026-02-11 23:36:58 +0200

    done second task in HW_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import {coursesAndDurationArray} from "./data/arrays-2.ts";
+import type {CoursesModel} from "./model/courses_model.ts";
 
 function App()
 {
 
-  return (
+  return(
     <>
-
+      <ul>{coursesAndDurationArray.map((value:CoursesModel,index:number) =><li key={index}>{value.title}, {value.monthDuration}</li>)}</ul>
     </>
   )
 }
---
+++ src/model/courses_model.ts
@@ -0,0 +1,5 @@
+export interface CoursesModel
+{
+    title:string;
+    monthDuration:number;
+}
