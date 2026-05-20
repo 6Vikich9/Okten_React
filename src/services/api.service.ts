5de780976fc408dba23757dd875546dfefee8bbe
6Vikich9 <v.hnetsko@gmail.com>
2026-05-21 01:15:42 +0300

    fix task in HW_4

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ import type {IDummyJsonUsersResponse} from "../models/IReqResInResponse.ts";
 
 export const getAllUsers = async (pg: string): Promise<IDummyJsonUsersResponse> => {
     const limit = 10;
-    const skip = (parseInt(pg) - 1) * limit; // Рахуємо відступ прямо тут
+    const skip = (parseInt(pg) - 1) * limit;
 
     return await fetch(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`)
         .then(value => value.json());
